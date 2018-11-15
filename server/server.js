a84d3bc76ae624bb7c95cca32457f2899cac2155
Zi Wang <zikevin.wang@mail.utoronto.ca>
2018-11-15 14:47:17 -0500

    node modules removed

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
-'use strict';
+"use strict";
 
-const koa = require('koa');
-const routers = require('./config/routes')
-const bodyParser = require('koa-bodyparser');
+const koa = require("koa");
+const routers = require("./config/routes");
+const bodyParser = require("koa-bodyparser");
 
 const app = new koa();
 app.use(bodyParser());
 
-
-app.use(routers.routes()).use(routers.allowedMethods())
+app.use(routers.routes()).use(routers.allowedMethods());
 
 app.listen(3000);
