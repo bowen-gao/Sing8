b72a417df53e24e972f73e15d00053cd6ff19bce
Zi Wang <zikevin.wang@mail.utoronto.ca>
2018-11-14 19:43:45 -0500

    model functions for Account table completed

--- server/models/account.js
+++ server/models/account.js
@@ -25,16 +25,77 @@ const viewOneAccount_By_Phone = phone => {
   });
 };
 
-const createOneAccount = () => {};
+const createOneAccount = (
+  email,
+  role,
+  country_code,
+  phone,
+  password,
+  last_login
+) => {
+  return new Promise((resolve, reject) => {
+    db.none(
+      "INSERT INTO Account(email,role,countrycode,phone,password,last_login) VALUES ($1, $2, $3, $4, $5, $6);",
+      [email, role, country_code, phone, password, last_login]
+    )
+      .then(data => {
+        resolve(data);
+      })
+      .catch(error => {
+        reject("ERROR:", error);
+      });
+  });
+};
 
-const updateAccount_Phone = email => {};
+const updateAccount_Phone = (email, country_code, phone) => {
+  return new Promise((resolve, reject) => {
+    db.none(
+      "UPDATE Account SET country_code = $1, phone = $2 WHERE email = $3;",
+      [country_code, phone, email]
+    )
+      .then(data => {
+        resolve(data);
+      })
+      .catch(error => {
+        console.log("ERROR:", error);
+        reject("ERROR:", error);
+      });
+  });
+};
 
-const deleteOneAccount = email => {};
+const updateAccount_Password = (email, password) => {
+  return new Promise((resolve, reject) => {
+    db.none("UPDATE Account SET password = $1 WHERE email = $2;", [
+      password,
+      email
+    ])
+      .then(data => {
+        resolve(data);
+      })
+      .catch(error => {
+        console.log("ERROR:", error);
+        reject("ERROR:", error);
+      });
+  });
+};
+
+const deleteOneAccount = email => {
+  return new Promise((resolve, reject) => {
+    db.none("DELETE FROM Account WHERE email = $1;", [email])
+      .then(data => {
+        resolve(data);
+      })
+      .catch(error => {
+        reject("ERROR:", error);
+      });
+  });
+};
 
 module.exports = {
   viewOneAccount_By_Email: viewOneAccount_By_Email,
   viewOneAccount_By_Phone: viewOneAccount_By_Phone,
   createOneAccount: createOneAccount,
   updateAccount_Phone: updateAccount_Phone,
-  deleteOneAccount: deleteOneAccount
+  deleteOneAccount: deleteOneAccount,
+  updateAccount_Password: updateAccount_Password
 };
