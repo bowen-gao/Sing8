3b3be56cc12bc99400c5dfab064b2d31a7e06d13
Zi Wang <zikevin.wang@mail.utoronto.ca>
2018-12-22 15:27:59 -0500

    xcode project initialized

---
+++ singer/singer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  singer
+//
+//  Created by Zi Wang on 2018/12/22.
+//  Copyright © 2018 ECE496. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
